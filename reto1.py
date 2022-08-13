6a8b3086b96d63f5a3781d355ef6ddef67498d58
209 <209@academicos.cesde.edu.co>
2022-08-12 21:01:16 -0500

    Se crean ejemplos de condicionales en python

--- reto1.py
+++ reto1.py
@@ -6,3 +6,9 @@ numero=int(input('Digite un numero: '))
 resultado= numero%3
 
 print(f'El modulo del numero digitado es: {resultado}')
+
+#CREANDO UN CONDICIONAL
+if(resultado==0):
+    print("es multiplo de 3")
+else:
+    print("No es multipo de 3")
